d92493c6f2d2973a840e4a671ce9a63cb4747b91
SDA <siluyanychev@gmail.com>
2024-01-03 12:52:08 +0100

    2 resx

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,25 +1,19 @@
 ﻿using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 using System;
-using System.Resources;
+using System.Collections.Generic;
 using System.Globalization;
+using System.Reflection;
+using System.Resources;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace AR_Finishings
 {
 
     public partial class MainWindow
     {
-        private ResourceManager rm = new ResourceManager("AR_Finishings.Strings", Assembly.GetExecutingAssembly())
+        private ResourceManager rm = new ResourceManager("AR_Finishings.Strings", Assembly.GetExecutingAssembly());
+        private CultureInfo cc = CultureInfo.CurrentCulture;
     }
 }
